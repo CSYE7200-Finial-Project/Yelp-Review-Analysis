5a121a99097d9e32cf8443cc41a3e17e86b5fb9d
yssdnj <mengdnj@gmail.com>
2016-04-03 02:48:30 -0400

    parser output
    output a file on desktop

--- src/main/scala/enu/edu/scala/finalyelp/JsonPaserFile.scala
+++ src/main/scala/enu/edu/scala/finalyelp/JsonPaserFile.scala
@@ -1,18 +1,15 @@
-package fpinscala.z
+package enu.edu.scala.finalyelp
 import play.api.libs.json._
 import scala.io._
 import scala.collection.mutable
-/*
- * this file can clean the reviews of German business 
- * and print out the text of the review context of the other country except Germany City Karlsruhe.
- * @author:yssdnj
- */
+import java.io._
+
 object JsonPaserFile extends App {
   val stream_business  = getClass.getResourceAsStream("/business_test.json")
   val stream_review  = getClass.getResourceAsStream("/review_test.json")
   val source_business = Source.fromInputStream(stream_business).getLines
   val source_review = Source.fromInputStream(stream_review).getLines
-  
+  val writer = new PrintWriter(new File("/Users/wanlima/Desktop/ouput_review.csv" )) 
   //  val json1: JsValue = Json.parse(source)
   var counter = 0;
   val karlsruhe_id = mutable.Set.empty[String]
@@ -32,8 +29,10 @@ object JsonPaserFile extends App {
     val business_id = (text \ "business_id").get.toString()
     if (!karlsruhe_id.exists(x => x.equals(business_id))) {
       println((text \ "text").get)
+      writer.write((text \ "text").get.toString + "\n")
       counter = counter + 1
     }
   }
+  writer.close()
   println(counter)
 }
