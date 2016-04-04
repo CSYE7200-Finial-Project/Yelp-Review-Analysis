390b0905581765833ea695713766b78bc4adf96b
yssdnj <mengdnj@gmail.com>
2016-04-04 01:34:29 -0400

    parse revise2
    seperate output(review context) in 5 files based on different stars

--- src/main/scala/neu/edu/scala/finalyelp/JsonPaserFile.scala
+++ src/main/scala/neu/edu/scala/finalyelp/JsonPaserFile.scala
@@ -4,12 +4,18 @@ import scala.io._
 import scala.collection.mutable
 import java.io._
 
+
 object JsonPaserFile extends App {
   val stream_business  = getClass.getResourceAsStream("/business_test.json")
   val stream_review  = getClass.getResourceAsStream("/review_test.json")
   val source_business = Source.fromInputStream(stream_business).getLines
   val source_review = Source.fromInputStream(stream_review).getLines
-  val writer = new PrintWriter(new File("src/main/resources/test200.csv" )) 
+  
+  val writer5 = new PrintWriter(new File("src/main/resources/test5.csv")) 
+  val writer4 = new PrintWriter(new File("src/main/resources/test4.csv")) 
+  val writer3 = new PrintWriter(new File("src/main/resources/test3.csv")) 
+  val writer2 = new PrintWriter(new File("src/main/resources/test2.csv")) 
+  val writer1 = new PrintWriter(new File("src/main/resources/test1.csv")) 
   //  val json1: JsValue = Json.parse(source)
   var counter = 0;
   val karlsruhe_id = mutable.Set.empty[String]
@@ -38,11 +44,22 @@ object JsonPaserFile extends App {
     if (!karlsruhe_id.exists(x => x.equals(business_id))) {
       print((text \ "stars").get.toString + " ")
       println((text \ "text").get.toString.replace("""\n""", " "))
-      writer.write((text \ "stars").get.toString + " " + (text \ "text").get.toString.replace("""\n""", " ") + "\n")
+      val stars = (text \ "stars").get.toString() match{
+        case "5" => writer5.write((text \ "text").get.toString.replace("""\n""", " ") + "\n")
+        case "4" => writer4.write((text \ "text").get.toString.replace("""\n""", " ") + "\n")
+        case "3" => writer3.write((text \ "text").get.toString.replace("""\n""", " ") + "\n")
+        case "2" => writer2.write((text \ "text").get.toString.replace("""\n""", " ") + "\n")
+        case "1" => writer1.write((text \ "text").get.toString.replace("""\n""", " ") + "\n")
+        
+      }
       counter = counter + 1
     }
   }
-  writer.close()
+  writer5.close()
+  writer4.close()
+  writer3.close()
+  writer2.close()
+  writer1.close()
   println(counter)
   
   
